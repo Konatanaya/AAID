6c95bdeb1fff3f49c701895181a85c5f021ff561
Konatanaya <wukonatan@gmail.com>
2022-02-10 01:39:45 +0800

    update

--- plot.py
+++ plot.py
@@ -100,18 +100,18 @@ def line_chart_diff_k(args):
         results[k] = np.loadtxt(path)
 
     legend = []
-    x = np.arange(0, args.time_steps, 1)
+    x = np.arange(1, args.time_steps, 1)
     plt.figure()
     for k in k_settings.keys():
         if k not in results:
             continue
         value = results[k].T
-        plt.plot(x, value[0], ms=6, color=k_settings[k]['color'], mec='black')
+        plt.plot(x, value[0][1:], ms=6, color=k_settings[k]['color'], mec='black')
         # plt.fill_between(x, value[0], value[2], color=k_settings[k]['color'], alpha=0.2)
         legend.append(k_settings[k]['name'])
     plt.legend(legend, fontsize=fontsize, loc='lower right', ncol=1, facecolor='white', fancybox=True, framealpha=0.4)
-    # plt.xticks(np.arange(0, length + 1, 25), fontsize=12)
-    # plt.yticks(np.arange(dataset_config['y_min'], dataset_config['y_max'] + 1, dataset_config['y_interval']), fontsize=12)
+    plt.xticks(np.arange(1, args.time_steps + 1, 25), fontsize=12)
+    # plt.yticks(np.arange(0.7, 1, 0.05), fontsize=12)
     plt.xlabel('Time steps', fontsize=fontsize)
     plt.ylabel('Filter bubble', fontsize=fontsize)
     plt.title('Filter bubble in ' + args.dataset + ' with CB deployed')
@@ -124,8 +124,10 @@ def line_chart_diff_k(args):
         if k not in results:
             continue
         value = results[k].T
-        plt.plot(x, value[1], ms=6, color=k_settings[k]['color'], mec='black')
+        plt.plot(x, value[1][1:], ms=6, color=k_settings[k]['color'], mec='black')
     plt.legend(legend, fontsize=fontsize, loc='lower right', ncol=1, facecolor='white', fancybox=True, framealpha=0.4)
+    plt.xticks(np.arange(0, args.time_steps + 1, 25), fontsize=12)
+    plt.yticks(np.arange(0.7, 1, 0.05), fontsize=12)
     plt.xlabel('Time steps', fontsize=fontsize)
     plt.ylabel('Echo chamber', fontsize=fontsize)
     plt.title('Echo chamber in ' + args.dataset + ' with CB deployed')
